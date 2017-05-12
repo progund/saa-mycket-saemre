9f322622810ab76d8851966b0f19640ba42a39b2
Rikard Fröberg <rikard@morus.se>
2017-05-12 15:54:56 +0200

    Removed test run comments

--- deltaevling-1/starting-point/TestPerson.java
+++ deltaevling-1/starting-point/TestPerson.java
@@ -44,37 +44,3 @@ public class TestPerson {
   }
 
 }
-  /*
-Test runs:
-$ javac Person.java && java TestPerson
-Welcome.
-Quit by entering 'sluta' as the name.
-Name: Henrik
-Age: 46
-Weight (use . as decimal point): 85.2
-Name: Rikard
-Age: 45
-Weight (use . as decimal point): 85
-Name: sluta
-[Henrik 46 years old 85.2 kg, Rikard 45 years old 85.0 kg]
-
-// Enter zero persons:
-$ javac Person.java && java TestPerson
-Welcome.
-Quit by entering 'sluta' as the name.
-Name: sluta
-[]
-
-// Proof of why we really should have error handling:
-$ javac Person.java && java TestPerson
-Welcome.
-Quit by entering 'sluta' as the name.
-Name: 1123
-Age: lastgammal
-Exception in thread "main" java.lang.NumberFormatException: For input string: "lastgammal"
-at java.lang.NumberFormatException.forInputString(NumberFormatException.java:65)
-at java.lang.Integer.parseInt(Integer.java:580)
-at java.lang.Integer.parseInt(Integer.java:615)
-at TestPerson.initPerson(Person.java:74)
-at TestPerson.main(Person.java:83)
-  */
